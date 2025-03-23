22bac20b31f765ef09ad3a3fa16d32242913054d
gyusty <andresgiusti93@gmail.com>
2025-03-23 18:25:01 -0500

    Fixed bug in stock service, that replicates call when items were removed

--- app/services/stock_service.py
+++ app/services/stock_service.py
@@ -4,12 +4,13 @@ from typing import List
 from app.repositories.Item import Item
 
 def validate_and_update_stock(order_items: List[dict], repository: Item):
+    
     for ordered_item in order_items:
-        item_name = ordered_item['name']
-        ordered_quantity = ordered_item['quantity']
-        
         if not ordered_item.get('canUpdate', False):
             continue
+
+        item_name = ordered_item['name']
+        ordered_quantity = ordered_item['quantity']
         
         stock_item = repository.find(item_name)
         if not stock_item:
@@ -17,11 +18,12 @@ def validate_and_update_stock(order_items: List[dict], repository: Item):
         if stock_item.quantity < ordered_quantity:
             raise HTTPException(status_code=400, detail=f"Not enough stock for item '{item_name}'")
 
-        for ordered_item in order_items:
-            if ordered_item.get('canUpdate', False):
-                new_quantity = stock_item.quantity - ordered_item['quantity']
-                repository.update_stock(ordered_item['name'], -ordered_item['quantity'])
-                
-                if new_quantity <= 0:
-                    print(f"Removing item: {ordered_item['name']} from stock (quantity is 0 or less)")
-                    repository.delete_item(ordered_item['name'])
+    for ordered_item in order_items:
+        if ordered_item.get('canUpdate', False):
+            repository.update_stock(ordered_item['name'], -ordered_item['quantity'])
+            updated_item = repository.find(ordered_item['name'])
+
+            if updated_item and updated_item.quantity <= 0:
+                print(f"Removing item: {ordered_item['name']} from stock (quantity is 0 or less)")
+                repository.delete_item(ordered_item['name'])
+
